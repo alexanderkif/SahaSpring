926688eb5f50a91de08e810b52310fc0cb8bcc74
alexanderkif <alexander_kif@mail.ru>
2017-09-24 16:19:02 +0400

    Move Initializer in root package

--- src/main/java/ga/saha/Initializer.java
+++ src/main/java/ga/saha/Initializer.java
@@ -1,11 +1,9 @@
-package ga.saha.config;
+package ga.saha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("ga.saha")
 public class Initializer {
         public static void main(String[] args) {
             SpringApplication.run(Initializer.class, args);
